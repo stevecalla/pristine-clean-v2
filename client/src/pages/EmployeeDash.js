c97c548404bc44bbaeeaeb153e7591950853c937
Steve Calla <callasteven@gmail.com>
2022-11-28 14:38:44 -0700

    Add user query to employee dash

--- client/src/pages/EmployeeDash.js
+++ client/src/pages/EmployeeDash.js
@@ -10,9 +10,20 @@ import { QUERY_LOCATIONS } from "../utils/queries";
 import EmployeeLocationsCont from "../components/EmployeeLocationsCont";
 import FullCalendar from "../components/FullCalendar";
 
+import { QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+
 const EmployeeDash = () => {
   const userId = getUserId();
   console.log(userId)
+
+    // get user info to render to page
+    const { loading, data } = useQuery(QUERY_ME, {
+      variables: { id: userId },
+      // if skip is true, this query will not be executed; in this instance, if the user is not logged in this query will be skipped when the component mounts
+      skip: !Auth.loggedIn(),
+    });
+    console.log({data}, loading);
 
   // Execute the query on component load
   // const { loading, data } = useQuery(QUERY_SINGLE_EMPLOYEE);
