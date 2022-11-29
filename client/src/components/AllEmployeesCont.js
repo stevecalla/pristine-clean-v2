5667d38c27a3a7a6bcc0877695adb740f6cbde97
PatrickARatcliff <pratcliff5@gmail.com>
2022-11-28 21:49:57 -0700

    hardcode data into dom

--- client/src/components/AllEmployeesCont.js
+++ client/src/components/AllEmployeesCont.js
@@ -15,7 +15,7 @@ const AllEmployeesCont = () => {
 
   return (
     <>
-      {employees.map((employee) => (
+      {/* {employees.map((employee) => (
         <Card>
           <Card.Header>
             {employee.firstName}, {employee.lastName}
@@ -29,7 +29,72 @@ const AllEmployeesCont = () => {
             </ListGroup>
           </Card.Body>
         </Card>
-      ))}
+      ))} */}
+      <Card className="m-2">
+          <Card.Header>
+            Ratcliff, Patrick
+          </Card.Header>
+          <Card.Body className=" bg-light">
+            <ListGroup variant="flush">
+              <ListGroup.Item>ID: 12345</ListGroup.Item>
+              <ListGroup.Item>303-123-4567</ListGroup.Item>
+              <ListGroup.Item>pratcliff5@gmail.com</ListGroup.Item>
+              <ListGroup.Item></ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <Card className="m-2">
+          <Card.Header>
+            Cleveland, Alex
+          </Card.Header>
+          <Card.Body className=" bg-light">
+            <ListGroup variant="flush">
+              <ListGroup.Item>ID: 54321</ListGroup.Item>
+              <ListGroup.Item>720-123-5678</ListGroup.Item>
+              <ListGroup.Item>alex@alex.com</ListGroup.Item>
+              <ListGroup.Item></ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <Card className="m-2">
+          <Card.Header>
+            Mcnatt, Colin
+          </Card.Header>
+          <Card.Body className=" bg-light">
+            <ListGroup variant="flush">
+              <ListGroup.Item>ID: 54321</ListGroup.Item>
+              <ListGroup.Item>303-098-7654</ListGroup.Item>
+              <ListGroup.Item>colin@colin.com</ListGroup.Item>
+              <ListGroup.Item></ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <Card className="m-2">
+          <Card.Header>
+            Calla, Steve
+          </Card.Header>
+          <Card.Body className=" bg-light">
+            <ListGroup variant="flush">
+              <ListGroup.Item>ID: 54321</ListGroup.Item>
+              <ListGroup.Item>303-098-7654</ListGroup.Item>
+              <ListGroup.Item>steve@steve.com</ListGroup.Item>
+              <ListGroup.Item></ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <Card className="m-2">
+          <Card.Header>
+            Fittipaldi, Emerson
+          </Card.Header>
+          <Card.Body className=" bg-light">
+            <ListGroup variant="flush">
+              <ListGroup.Item>ID: 000000</ListGroup.Item>
+              <ListGroup.Item>970-456-1234</ListGroup.Item>
+              <ListGroup.Item>fitii@fitti.com</ListGroup.Item>
+              <ListGroup.Item></ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
     </>
   );
 };
--- client/src/pages/Location.js
+++ client/src/pages/Location.js
@@ -1,10 +1,13 @@
 import React from "react";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+
+import Map from '../components/Map'
 
 import { useQuery } from "@apollo/client";
 // query a single location
@@ -18,6 +21,50 @@ const Location = () => {
   const location = locationData?.business || [];
   return (
     <main>
+      {/* <Container>
+        <Row>
+          <Col> */}
+      {/* h1 location.business */}
+      {/*  li location.manager */}
+      {/*  li location.cleaners */}
+      {/*  li location.cleaners */}
+      {/* <Card>
+              <Card.Header>{location.business}</Card.Header>
+              <Card.Body className=" bg-light">
+                <ListGroup variant="flush">
+                  <ListGroup.Item>{location.manager}</ListGroup.Item>
+                  <ListGroup.Item>{location.cleaners}</ListGroup.Item>
+                  <ListGroup.Item>{location.frequency}</ListGroup.Item>
+                </ListGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col> */}
+      {/*  h1 INSTRUCTIONs */}
+      {/*  li location.instructions */}
+      {/* <Card>
+              <Card.Header>Instructions</Card.Header>
+              <Card.Body className=" bg-light">
+                <ListGroup variant="flush">
+                  <ListGroup.Item>{location.instructions}</ListGroup.Item>
+                </ListGroup>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row> */}
+      <Col>{/* MAP GOES HERE */}</Col>
+      {/* </Row>
+        <Row>
+          <Col>
+            <Button type="click" className="btn-primary">
+              Get Directions
+            </Button>
+          </Col>
+        </Row>
+      </Container> */}
       <Container>
         <Row>
           <Col>
@@ -26,12 +73,25 @@ const Location = () => {
             {/*  li location.cleaners */}
             {/*  li location.cleaners */}
             <Card>
-              <Card.Header>{location.business}</Card.Header>
+              <Card.Header><h1>Walmart</h1></Card.Header>
               <Card.Body className=" bg-light">
                 <ListGroup variant="flush">
-                  <ListGroup.Item>{location.manager}</ListGroup.Item>
-                  <ListGroup.Item>{location.cleaners}</ListGroup.Item>
-                  <ListGroup.Item>{location.frequency}</ListGroup.Item>
+                  <ListGroup.Item>Manager: Mcnatt, Colin</ListGroup.Item>
+                  <ListGroup.Item>Address: 2468 Xanadu Circle, Narnia 08412</ListGroup.Item>
+                  <ListGroup.Item>Cleaners
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>Ratcliff, Patrick</ListGroup.Item>
+                      <ListGroup.Item>Calla, Steve</ListGroup.Item>
+                      <ListGroup.Item>Cleveland, Alex</ListGroup.Item>
+                    </ListGroup>
+                  </ListGroup.Item>
+                  <ListGroup.Item>Frequency:
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>Tuesday</ListGroup.Item>
+                      <ListGroup.Item>Friday</ListGroup.Item>
+                      <ListGroup.Item>Sunday</ListGroup.Item>
+                    </ListGroup>
+                  </ListGroup.Item>
                 </ListGroup>
               </Card.Body>
             </Card>
@@ -45,25 +105,44 @@ const Location = () => {
               <Card.Header>Instructions</Card.Header>
               <Card.Body className=" bg-light">
                 <ListGroup variant="flush">
-                  <ListGroup.Item>{location.instructions}</ListGroup.Item>
+                  <ListGroup.Item>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</ListGroup.Item>
                 </ListGroup>
+              </Card.Body>
+              <Card.Body>
+              <Map />
               </Card.Body>
             </Card>
           </Col>
         </Row>
         <Row>
-          <Col>{/* MAP GOES HERE */}</Col>
+          {/* MAP GOES HERE */}
+          <Col >
+          
+            
+            
+          </Col>
         </Row>
-        <Row>
+        
+        {/* <Row>
           <Col>
             <Button type="click" className="btn-primary">
               Get Directions
             </Button>
           </Col>
-        </Row>
+        </Row> */}
       </Container>
+
     </main>
   );
 };
 
 export default Location;
+
+const mapContainer = {
+  flexDirection: "column",
+  width: "100%",
+  height: "100%",
+  position: "relative",
+  margin: 0,
+  padding: 0
+};
