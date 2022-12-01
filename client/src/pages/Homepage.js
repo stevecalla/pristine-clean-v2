7446691889e0570d90ac01580b18d5b0818ac5ba
Steve Calla <callasteven@gmail.com>
2022-11-30 22:15:30 -0700

    Initial draft

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import firstslide from "../assets/firstslide.svg";
+import secondslide from "../assets/secondslide.svg";
+import thirdslide from "../assets/thirdslide.svg";
+import LoginFormCopy from "../components/LoginFormCopy";
+import SignupFormCopy from "../components/SignupFormCopy";
 
 const Homepage = () => {
+  // set slide prop to true to enable animation
   return (
-    <div>Homepage</div>
-  )
-}
+    <>
+      <Carousel className="m-3 p-3" slide={false}>
+        <Carousel.Item>
+          <img className="d-block w-100" src={firstslide} alt="First slide" />
+          <Carousel.Caption>
+            {/* <h3>First slide label</h3> */}
+            {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={secondslide} alt="Second slide" />
+
+          <Carousel.Caption>
+            {/* <h3>Second slide label</h3> */}
+            {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={thirdslide} alt="Third slide" />
+
+          <Carousel.Caption>
+            {/* <h3>Third slide label</h3> */}
+            <p>
+              {/* Praesent commodo cursus magna, vel scelerisque nisl consectetur. */}
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      <LoginFormCopy />
+
+      {/* <SignupFormCopy /> */}
+    </>
+  );
+};
 
 export default Homepage;
