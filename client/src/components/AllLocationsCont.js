72bdf75fceb4f3be640fddfca8ea9a1cc0d32885
Alex Cleveland <Alexcleveland5@gmail.com>
2022-11-30 17:12:29 -0700

    Location data on dash working

--- client/src/components/AllLocationsCont.js
+++ client/src/components/AllLocationsCont.js
@@ -17,75 +17,76 @@ const AllLocationsCont = () => {
     variables: { id: userId },
     skip: !Auth.loggedIn(),
   });
-  if (!loading) {
-    console.log(data)
-  };
-  console.log(data);
 
-  return (
-    <>
-      {/* h1 location.business */}
-      {/*  li location.manager */}
-      {/*  li location.cleaners */}
-      {/*  li location.cleaners */}
-      {/* {locations.map((location) => (
-        <Card>
-          <Card.Header>{location.businessName}</Card.Header>
+  const locations = data.me.locations;
+  console.log(locations);
+
+  if (!loading) {
+    return (
+      <>
+        {/* h1 location.business */}
+        {/*  li location.manager */}
+        {/*  li location.cleaners */}
+        {/*  li location.cleaners */}
+        {locations.map((location) => (
+          <Card>
+            <Card.Header>{location.businessName}</Card.Header>
+            <Card.Body className=" bg-light">
+              <ListGroup variant="flush">
+                <ListGroup.Item>Address: {location.address}</ListGroup.Item>
+                <ListGroup.Item>Days: {location.shifts}</ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        ))}
+        {/* <Card className="m-2">
+          <Card.Header>Walmart</Card.Header>
           <Card.Body className=" bg-light">
             <ListGroup variant="flush">
-              <ListGroup.Item>{location._id}</ListGroup.Item>
-              <ListGroup.Item>{location.address}</ListGroup.Item>
+              <ListGroup.Item>ID: 123456</ListGroup.Item>
+              <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
             </ListGroup>
           </Card.Body>
         </Card>
-      ))} */}
-      <Card className="m-2">
-        <Card.Header>Wal-Mart</Card.Header>
-        <Card.Body className=" bg-light">
-          <ListGroup variant="flush">
-            <ListGroup.Item>ID: 123456</ListGroup.Item>
-            <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
-          </ListGroup>
-        </Card.Body>
-      </Card>
-      <Card className="m-2">
-        <Card.Header>Target</Card.Header>
-        <Card.Body className=" bg-light">
-          <ListGroup variant="flush">
-            <ListGroup.Item>ID: 123456</ListGroup.Item>
-            <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
-          </ListGroup>
-        </Card.Body>
-      </Card>
-      <Card className="m-2">
-        <Card.Header>K-Mart</Card.Header>
-        <Card.Body className=" bg-light">
-          <ListGroup variant="flush">
-            <ListGroup.Item>ID: 123456</ListGroup.Item>
-            <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
-          </ListGroup>
-        </Card.Body>
-      </Card>
-      <Card className="m-2">
-        <Card.Header>Big Lots</Card.Header>
-        <Card.Body className=" bg-light">
-          <ListGroup variant="flush">
-            <ListGroup.Item>ID: 123456</ListGroup.Item>
-            <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
-          </ListGroup>
-        </Card.Body>
-      </Card>
-      <Card className="m-2">
-        <Card.Header>Dollar General</Card.Header>
-        <Card.Body className=" bg-light">
-          <ListGroup variant="flush">
-            <ListGroup.Item>ID: 123456</ListGroup.Item>
-            <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
-          </ListGroup>
-        </Card.Body>
-      </Card>
-    </>
-  );
+        <Card className="m-2">
+          <Card.Header>Target</Card.Header>
+          <Card.Body className=" bg-light">
+            <ListGroup variant="flush">
+              <ListGroup.Item>ID: 123456</ListGroup.Item>
+              <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <Card className="m-2">
+          <Card.Header>K-Mart</Card.Header>
+          <Card.Body className=" bg-light">
+            <ListGroup variant="flush">
+              <ListGroup.Item>ID: 123456</ListGroup.Item>
+              <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <Card className="m-2">
+          <Card.Header>Big Lots</Card.Header>
+          <Card.Body className=" bg-light">
+            <ListGroup variant="flush">
+              <ListGroup.Item>ID: 123456</ListGroup.Item>
+              <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card>
+        <Card className="m-2">
+          <Card.Header>Dollar General</Card.Header>
+          <Card.Body className=" bg-light">
+            <ListGroup variant="flush">
+              <ListGroup.Item>ID: 123456</ListGroup.Item>
+              <ListGroup.Item>1234 Cedar Wood Ln</ListGroup.Item>
+            </ListGroup>
+          </Card.Body>
+        </Card> */}
+      </>
+    )
+  };
 };
 
 export default AllLocationsCont;
