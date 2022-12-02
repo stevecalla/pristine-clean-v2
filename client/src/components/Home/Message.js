6c5a3a5b1bec221b287e6273dac4f83da55e522f
Steve Calla <callasteven@gmail.com>
2022-12-02 09:36:09 -0700

    Add qr code

--- client/src/components/Home/Message.js
+++ client/src/components/Home/Message.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import mopClean from "../../assets/mopClean.png";
+import qrcode_dry_sands.png from "../../assets/mopClean.png";
 
 const Message = () => {
   return (
-    <div className="d-flex justify-content-center mt-5">
+    <div className="d-flex flex-column justify-content-center align-items-center overflow-auto mt-5">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
       <Card style={{ width: "18rem" }}>
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
@@ -22,7 +36,6 @@ const Message = () => {
           >
             Pristine Clean
           </Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
         </Card.Body>
       </Card>
     </div>
