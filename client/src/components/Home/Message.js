96b5f572b5104663dd6fa0a3ba963d9aa559c772
Steve Calla <callasteven@gmail.com>
2022-12-02 09:47:13 -0700

    Add QR code

--- client/src/components/Home/Message.js
+++ client/src/components/Home/Message.js
@@ -2,24 +2,30 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import mopClean from "../../assets/mopClean.png";
-import qrcode_dry_sands.png from "../../assets/mopClean.png";
+import qrcode_dry_sands from "../../assets/qrcode_dry_sands.png";
 
 const Message = () => {
   return (
-    <div className="d-flex flex-column justify-content-center align-items-center overflow-auto mt-5">
+    <div className="d-flex flex-column align-items-center mt-3 mb-3">
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+        <Card.Img variant="top" src={qrcode_dry_sands} />
+        <Card.Body className="pt-0">
+          <Card.Title>Download</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Amazing employee app!!
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          {/* <Button variant="primary">Go somewhere</Button> */}
+          <Card.Link
+            href="https://www.pristinecleanbycolin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Pristine Clean
+          </Card.Link>
         </Card.Body>
       </Card>
 
-      <Card style={{ width: "18rem" }}>
+      {/* <Card style={{ width: "18rem" }}>
         <Card.Body>
           <Card.Title>Card Title</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">
@@ -37,7 +43,7 @@ const Message = () => {
             Pristine Clean
           </Card.Link>
         </Card.Body>
-      </Card>
+      </Card> */}
     </div>
   );
 };
