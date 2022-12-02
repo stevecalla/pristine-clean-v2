6ddf230aeb237004b6832d62de3f3beb824693d8
Alex Cleveland <Alexcleveland5@gmail.com>
2022-12-01 17:48:15 -0700

    Cleaned up location

--- client/src/pages/Location.js
+++ client/src/pages/Location.js
@@ -12,11 +12,6 @@ import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed';
 
 import Map from '../components/Map';
 
-// import { useQuery } from "@apollo/client";
-// query a single location
-// import { QUERY_SINGLE_LOCATION } from "../utils/queries";
-
-// const Location = ({ location.id }) => {
 const Location = ({ locationDetails }) => {
 
   console.log(locationDetails)
@@ -32,92 +27,6 @@ const Location = ({ locationDetails }) => {
 
   return (
     <main>
-      {/* <Container className="my-2">
-        <Button
-          onClick={() => setOpenDetails(!openDetails)}
-          aria-controls="details-fade-text"
-          aria-expanded={openDetails}
-          size="lg"
-          className="btn-block my-2"
-        >
-          View Location Details
-        </Button>
-        <Collapse
-          in={openDetails}
-        >
-          <div id="collapse-details-bar">
-            <Row>
-              <Col>
-                <Card>
-                  <Card.Header>{location.businessName}</Card.Header>
-                  <Card.Body className=" bg-light">
-                    <ListGroup variant="flush">
-                      <ListGroup.Item>{location.manager}</ListGroup.Item>
-                      <ListGroup.Item>{location.address}</ListGroup.Item>
-                      <ListGroup.Item>{location.businessContact}</ListGroup.Item>
-                      <ListGroup.Item>{location.cleaners}</ListGroup.Item>
-                      <ListGroup.Item>{location.shifts}</ListGroup.Item>
-                      <ListGroup.Item>
-                        <Button
-                          onClick={() => setOpenInstruction(!openInstructions)}
-                          aria-controls="instructions-fade-text"
-                          aria-expanded={openInstructions}
-                          size="lg"
-                          className="btn-block my-2"
-                        >
-                          View Instructions
-                        </Button>
-                        <Collapse
-                          style={{ 'height': '300px', 'overflow': 'scroll!important' }}
-                          in={openInstructions}
-                        >
-                          <div id="collapse-instructions-bar">
-                            <Card>
-                              <Card.Body className=" bg-light">
-                                <ListGroup variant="flush">
-                                  <ListGroup.Item>Facility Type: {location.instructions.facilityType}</ListGroup.Item>
-                                  <ListGroup.Item>Cleaning Type: {location.instructions.cleaningType}</ListGroup.Item>
-                                  <ListGroup.Item>Bathrooms: {location.instructions.bathrooms}</ListGroup.Item>
-                                  <ListGroup.Item>Lobby: {location.instructions.lobby}</ListGroup.Item>
-                                  <ListGroup.Item>Sitting-Area: {location.instructions.sittingArea}</ListGroup.Item>
-                                  <ListGroup.Item>Break-Room: {location.instructions.breakRoom}</ListGroup.Item>
-                                  <ListGroup.Item>Fornt-Desk: {location.instructions.frontdesk}</ListGroup.Item>
-                                  <ListGroup.Item>Appliances: {location.instructions.appliances}</ListGroup.Item>
-                                  <ListGroup.Item>Dusting: {location.instructions.dusting}</ListGroup.Item>
-                                  <ListGroup.Item>Windows: {location.instructions.windows}</ListGroup.Item>
-                                  <ListGroup.Item>Trash: {location.instructions.trash}</ListGroup.Item>
-                                  <ListGroup.Item>Vacuum: {location.instructions.vacuum}</ListGroup.Item>
-                                  <ListGroup.Item>Mop: {location.instructions.mop}</ListGroup.Item>
-                                  <ListGroup.Item>Additional Services: {location.instructions.additionalServices}</ListGroup.Item>
-                                  <ListGroup.Item>Exclusions: {location.instructions.exclusions}</ListGroup.Item>
-                                </ListGroup >
-                              </Card.Body >
-                            </Card >
-                          </div >
-                        </Collapse >
-                      </ListGroup.Item>
-                    </ListGroup>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </div>
-        </Collapse>
-        <Row>
-          <Col>
-            <Card>
-              <Card.Body>
-                <ResponsiveEmbed className='mt-1' style={{ height: '1000px' }}>
-                  <div>
-                    <Map />
-                  </div>
-                </ResponsiveEmbed>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container> */}
-
 
       <Container className="my-2">
         <Button
@@ -141,7 +50,7 @@ const Location = ({ locationDetails }) => {
                     <ListGroup variant="flush">
                       <ListGroup.Item>Manager: McNatt, Colin</ListGroup.Item>
                       <ListGroup.Item>Address: {locationDetails.address}</ListGroup.Item>
-                      <ListGroup.Item>Contact #: {locationDetails.businessnContact}</ListGroup.Item>
+                      <ListGroup.Item>Contact #: {locationDetails.businessContact}</ListGroup.Item>
                       <ListGroup.Item>Cleaners: Patrick, Steve, Alex</ListGroup.Item>
                       <ListGroup.Item>Frequency: {locationDetails.shifts}</ListGroup.Item>
                       <Button
