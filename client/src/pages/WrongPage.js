51448d260926a9dd656d1cef4b07b525f128c960
Steve Calla <callasteven@gmail.com>
2022-12-03 19:41:40 -0700

    Adjust asset name

--- client/src/pages/WrongPage.js
+++ client/src/pages/WrongPage.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import WrongPageImg from '../assets/WrongPage.png'
+import wrongPageImg from "../assets/wrongPage.png";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
@@ -8,24 +8,33 @@ import { Link, useNavigate } from "react-router-dom";
 
 const WrongPage = () => {
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        setTimeout(() => navigate("/dashboard"), 5000);
-    }, [] )
+  useEffect(() => {
+    setTimeout(() => navigate("/dashboard"), 5000);
+  }, []);
 
-
-
-    return (
-        <Container>
-            <Row className=" justify-content-center align-items-center" style={{ minHeight: '80vh' }}>
-                <Button as={Link} to="/dashboard" className="rounded-pill shadow-lg" style={{ backgroundColor: 'black' }}>
-                    <img src={WrongPageImg} alt="404 Wrong Page" style={{ maxHeight: '40vh' }} />
-                </Button>
-            </Row>
-        </Container>
-    )
-}
-
+  return (
+    <Container>
+      <Row
+        className=" justify-content-center align-items-center"
+        style={{ minHeight: "80vh" }}
+      >
+        <Button
+          as={Link}
+          to="/dashboard"
+          className="rounded-pill shadow-lg"
+          style={{ backgroundColor: "black" }}
+        >
+          <img
+            src={wrongPageImg}
+            alt="404 Wrong Page"
+            style={{ maxHeight: "40vh" }}
+          />
+        </Button>
+      </Row>
+    </Container>
+  );
+};
 
 export default WrongPage;
