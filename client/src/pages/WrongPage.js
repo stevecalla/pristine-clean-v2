7d592e46a08c21d7d7fc95531a8996c4da693050
colinmichael89 <colinmichael89@gmail.com>
2022-12-05 09:38:32 -0700

    typo wrongPage in page WrongPAge not capitailized

--- client/src/pages/WrongPage.js
+++ client/src/pages/WrongPage.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import wrongPageImg from "../assets/wrongPage.png";
-import { Container, Row, Button } from "react-bootstrap/";
-import "../styles/button-style.css";
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import wrongPageImg from '../assets/WrongPage.png';
+import { Container, Row, Button } from 'react-bootstrap/';
+import '../styles/button-style.css';
 
 const WrongPage = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    setTimeout(() => navigate("/dashboard"), 7000);
+    setTimeout(() => navigate('/dashboard'), 7000);
   }, []);
 
   return (
     <Container>
       <Row
-        className=" justify-content-center align-items-center"
-        style={{ minHeight: "80vh" }}
+        className=' justify-content-center align-items-center'
+        style={{ minHeight: '80vh' }}
       >
         <Button
           as={Link}
-          to="/dashboard"
-          className="rounded-pill wrong-page-button"
-          style={{ backgroundColor: "white", border: "none" }}
+          to='/dashboard'
+          className='rounded-pill wrong-page-button'
+          style={{ backgroundColor: 'white', border: 'none' }}
         >
           <img
             src={wrongPageImg}
-            alt="404 Wrong Page"
-            style={{ maxHeight: "40vh" }}
+            alt='404 Wrong Page'
+            style={{ maxHeight: '40vh' }}
           />
         </Button>
       </Row>
