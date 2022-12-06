2abb8c7fa47d2f2e9ec06f5895616d63c8d8cffa
Steve Calla <callasteven@gmail.com>
2022-12-05 21:04:30 -0700

    Delete old config with db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  // process.env.MONGODB_URI || "mongodb://localhost:27017/pristine-clean",
   process.env.MONGODB_URI || `mongodb://localhost:27017/${process.env.DB_NAME}`,
   {
     useNewUrlParser: true,
